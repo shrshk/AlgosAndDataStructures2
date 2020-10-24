91c1aea11ca7256e141be676df16b18741150d86
shirish <shirish101k@gmail.com>
2020-10-24 18:03:43 -0500

    Setting up algosDS2

---
+++ DatastructuresAndAlgorithms2/src/main/java/com/datastructuresAndAlgorithms/com/datastructuresAndAlgorithms/App.java
@@ -0,0 +1,13 @@
+package com.datastructuresAndAlgorithms.com.datastructuresAndAlgorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.datastructuresAndAlgorithms/src/defaultTestPackage/FirstProgram.java
@@ -0,0 +1,7 @@
+package defaultTestPackage;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
